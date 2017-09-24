b74400909e49b4871421936b6e77c9325e963f5a
ashantal <ashantal@hotmail.com>
2017-09-24 00:38:39 -0700

    setup assets

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var ws_server = require('./utils/websocket_server_side.js')({ block_delay: helpe
 var host = 'localhost';
 var port = helper.getListingsPort();
 var wss = {};
+var enrollObj = null;
+var listings_lib = null;
 
 // -- states --//
 var start_up_states = {												//Startup Steps
@@ -40,6 +42,21 @@ var start_up_states = {												//Startup Steps
 	find_chaincode: { state: 'waiting', step: 'step3' },			// Step 3 - find the chaincode on the channel
 	register_states: { state: 'waiting', step: 'step4' },			// Step 4 - create the listing states
 };
+// Message to client to communicate where we are in the start up
+function build_state_msg() {
+	return {
+		msg: 'app_state',
+		state: start_up_states,
+		first_setup: process.env.app_first_setup
+	};
+}
+// Send to all connected clients
+function broadcast_state(change_state, outcome) {
+	try {
+		start_up_states[change_state].state = outcome;
+		wss.broadcast(build_state_msg());								//tell client our app state
+	} catch (e) { }														//this is expected to fail for "checking"
+}
 
 // --- setup --- //
 app.set('views', path.join(__dirname, 'views'));
@@ -102,34 +119,37 @@ process.on('uncaughtException', function (err) {
 // ------------------------------------------------------------------------------------------------------------------------------
 // Life Starts Here!
 // ------------------------------------------------------------------------------------------------------------------------------
-process.env.app_first_setup = 'yes';				//init
-let config_error = helper.checkConfig();
+
 setupWebSocket();
 
+process.env.app_first_setup = 'yes';
+let config_error = helper.checkConfig();
 if (config_error) {
-	broadcast_state('checklist', 'failed');			//checklist step is done
+	broadcast_state('checklist', 'failed');			
 } else {
-	broadcast_state('checklist', 'success');		//checklist step is done
-}
+	broadcast_state('checklist', 'success');
 
-console.log('\n');
-//logger.info('Using settings in ' + process.env.creds_filename + ' to see if we have launched this app before...');
+	enroll_admin(1, function (e) {
+		if (e != null) {
+			logger.warn('Error enrolling admin');
+			broadcast_state('enrolling', 'failed');
+			startup_unsuccessful();
+		} else {
+			logger.info('Success enrolling admin');
+			broadcast_state('enrolling', 'success');
 
-// Message to client to communicate where we are in the start up
-function build_state_msg() {
-	return {
-		msg: 'app_state',
-		state: start_up_states,
-		first_setup: process.env.app_first_setup
-	};
-}
-
-// Send to all connected clients
-function broadcast_state(change_state, outcome) {
-	try {
-		start_up_states[change_state].state = outcome;
-		wss.broadcast(build_state_msg());								//tell client our app state
-	} catch (e) { }														//this is expected to fail for "checking"
+			setup_lib(function () {
+				detect_prev_startup({ startup: true }, function (err) {
+					if (err) {
+						startup_unsuccessful();
+					} else {
+						logger.debug('Detected that we have launched successfully before');
+						logger.debug('Welcome back - Initiating start up\n\n');
+					}
+				});
+			});
+		}
+	});	
 }
 
 // ============================================================================================================================
@@ -150,6 +170,27 @@ function setupWebSocket() {
 			catch (e) {
 				logger.debug('[ws] message error', message, e.stack);
 			}
+			if (data && data.type == 'setup') {
+				logger.debug('[ws] setup message', data);
+
+				//enroll admin
+				if (data.configure === 'enrollment') {
+					removeKVS();
+					helper.write(data);													//write new config data to file
+					enroll_admin(1, function (e) {
+						if (e == null) {
+							setup_lib(function () {
+								detect_prev_startup({ startup: false }, function (err) {
+									if (err) {
+										create_states(helper.getListingStates()); 	//builds marbles, then starts webapp
+									}
+								});
+							});
+						}
+					});
+				}
+			}
+						
 		});
 
 		ws.on('error', function (e) { logger.debug('[ws] error', e); });
@@ -171,4 +212,162 @@ function setupWebSocket() {
 			}
 		});
 	};
+}
+
+
+
+//1. Enroll an admin with the CA for this peer/channel
+function enroll_admin(attempt, cb) {
+	fcw.enroll(helper.makeEnrollmentOptions(0), function (errCode, obj) {
+		if (errCode != null) {
+			logger.error('could not enroll...');
+
+			// --- Try Again ---  //
+			if (attempt >= 2) {
+				if (cb) cb(errCode);
+			} else {
+				removeKVS();
+				enroll_admin(++attempt, cb);
+			}
+		} else {
+			enrollObj = obj;
+			if (cb) cb(null);
+		}
+	});
+}
+
+//2. setup library and check if cc is instantiated
+function setup_lib(cb) {
+	var opts = helper.makeListingsLibOptions();
+	listings_lib = require('./utils/listings_cc_lib.js')(enrollObj, opts, fcw, logger);
+	ws_server.setup(wss.broadcast, listings_lib);
+
+	logger.debug('Checking if chaincode is already instantiated or not');
+	const channel = helper.getChannelId();
+	const first_peer = helper.getFirstPeerName(channel);
+	var options = {
+		peer_urls: [helper.getPeersUrl(first_peer)],
+	};
+	listings_lib.check_if_already_instantiated(options, function (not_instantiated, enrollUser) {
+		if (not_instantiated) {									//if this is truthy we have not yet instantiated.... error
+			console.log('');
+			logger.debug('Chaincode was not detected: "' + helper.getChaincodeId() + '", all stop');
+			logger.debug('Open your browser to http://' + host + ':' + port + ' and login to tweak settings for startup');
+			process.env.app_first_setup = 'yes';				//overwrite state, bad startup
+			broadcast_state('find_chaincode', 'failed');
+		}
+		else {													//else we already instantiated
+			console.log('\n----------------------------- Chaincode found on channel "' + helper.getChannelId() + '" -----------------------------\n');
+			listings_lib.check_version(options, function (err, resp) {
+				if (helper.errorWithVersions(resp)) {
+					broadcast_state('find_chaincode', 'failed');
+				} else {
+					logger.info('Chaincode version is good');
+					broadcast_state('find_chaincode', 'success');
+					if (cb) cb(null);
+				}
+			});
+		}
+	});
+}
+
+
+//3. Find if this app started successfully before
+function detect_prev_startup(opts, cb) {
+	logger.info('Checking ledger for states listed in the config file');
+	listings_lib.read_everything(null, function (err, resp) {			//read the ledger for states
+		if (err != null) {
+			logger.warn('Error reading ledger');
+			if (cb) cb(true);
+		} else {
+			if (find_missing_states(resp)) {						//check if each state in the settings file has been created in the ledger
+				logger.info('We need to create states');			//there are states that do not exist!
+				broadcast_state('register_states', 'waiting');
+				if (cb) cb(true);
+			} else {
+				broadcast_state('register_states', 'success');			//everything is good
+				process.env.app_first_setup = 'no';
+				logger.info('Everything is in place');
+				if (cb) cb(null);
+			}
+		}
+	});
+}
+
+
+// Detect if there are states in the settings doc that are not in the ledger
+function find_missing_states(resp) {
+	return false;
+}
+
+// Create states
+function create_states(states) {
+	logger.info('Creating states');
+	if (states && states.length > 0) {
+		async.each(states, function (state, cb) {
+			logger.debug('- creating state: ', state);
+			create_state(state, function (errCode, resp) {
+				cb();
+			});
+		}, function (err) {
+			logger.info('finished creating states');
+			if (err == null) {
+				 all_done();								
+			}
+		});
+	}
+	else {
+		logger.debug('- there are no new states to create');
+		all_done();
+	}
+}
+
+// Create State
+function create_state(state, statetype, cbs) {
+	const channel = helper.getChannelId();
+	const first_peer = helper.getFirstPeerName(channel);
+	var options = {
+		peer_urls: [helper.getPeersUrl(first_peer)],
+		args: {
+			state_type: state.state_type,
+			state_name:''
+		}
+	};
+
+	async.each(state.state_names, function (state_name, cb) {
+		logger.debug('- creating state: ', state_name);
+		options.args.state_name = state_name;
+		listings_lib.register_state(options, function (e, resp) {
+			if (e != null) {
+				logger.error('error creating the state', e, resp);
+				cb(e, resp);
+			}
+			else {
+				cb(null, resp);
+			}
+		});			
+	});
+
+	cbs();
+}
+
+
+// Clean Up OLD KVS
+function removeKVS() {
+	try {
+		logger.warn('removing older kvs and trying to enroll again');
+		misc.rmdir(helper.getKvsPath({ going2delete: true }));			//delete old kvs folder
+		logger.warn('removed older kvs');
+	} catch (e) {
+		logger.error('could not delete old kvs', e);
+	}
+}
+
+// We are done, inform the clients
+function all_done() {
+	console.log('\n------------------------------------------ All Done ------------------------------------------\n');
+	broadcast_state('register_states', 'success');
+	process.env.app_first_setup = 'no';
+
+	ws_server.check_for_updates(null);									//call the periodic task to get the state of everything
 }
